4ff07ca12925257cf6566db780bb4d7e4b2e0636
Amit Patel <amitpatel_7@msn.com>
2018-09-26 20:19:15 -0700

    created d3.hierarchy object for json data and created d3.treemap for layout. Also generated and appended rect elements for each json data node

--- app.js
+++ app.js
@@ -3,6 +3,44 @@ fetch('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/da
     .then((response) => response.json())
     .then(
         (data) => {
-            const root = d3.hierarchy(data);
-            console.log(root)
+            // D3 requires hierarchial data to be in the form of a d3.hierarchy object
+            const rootNode = d3.hierarchy(data);
+
+            // Created treemap layout using D3's treemap method
+            const treemapLayout = d3.treemap()
+                                    .size([400, 200])
+                                    .paddingOuter(10);
+            
+            // Run sum method on the hierarchy - this traverses the tree and sets .value on each node to the sum of its children
+            rootNode.sum((d) => d.value);
+
+            // call treemapLayout and pass in hierachy object
+            treemapLayout(rootNode);
+
+            d3.select('svg g')
+              .selectAll('rect')
+              .data(rootNode.descendants())
+              .enter()
+              .append('rect')
+              .attr('x', (d) => d.x0)
+              .attr('y', (d) => d.y0)
+              .attr('width', (d) => d.x1 - d.x0)
+              .attr('height', (d) => d.y1 - d.y0);
+
+            console.log(rootNode);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         });
